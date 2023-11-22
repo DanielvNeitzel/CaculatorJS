17481e196f42443f2dabce7429a350baebacafa5
Daniel Neitzel <104405943+DanielvNeitzel@users.noreply.github.com>
2023-11-22 03:20:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ function clearDisplay() {
 }
 
 function calculate() {
-    if (document.getElementById('display').value !== 'Error') {
-        try {
-            document.getElementById('display').value = eval(document.getElementById('display').value);
-        } catch (error) {
+    if (document.getElementById('display').value !== '') {
+        if (document.getElementById('display').value !== 'Error') {
+            try {
+                document.getElementById('display').value = eval(document.getElementById('display').value);
+            } catch (error) {
+                alert('Não é possivel realizar calculo.');
+                document.getElementById('display').value = 'Error';
+            }
+        } else {
             alert('Não é possivel realizar calculo.');
-            document.getElementById('display').value = 'Error';
         }
-    } else {
-        alert('Não é possivel realizar calculo.');
     }
 }
